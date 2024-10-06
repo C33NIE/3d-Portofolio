eebe327f038732296da0ddf06037f21abb2bffbc
Muhammad Yaseen Raja (C33NIE) <rajayaseen2003@gmail.com>
2024-10-06 10:37:20 -0700

    name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,13 +4,15 @@ import Position from "./Position";
 const Hero = ({ scrollContainer }) => {
   return (
     <section className="parallax">
-    <div className='parallax__content absolute top-[10%] sm:top-[16%] lg:top-[24%] w-full mx-auto lg:pl-[38vh] lg:pr-[30vh] xl:pl-96 xl:pr-72 2xl:px-40 3xl:px-60 flex flex-col lg:flex-row items-start z-10'>
-        <div className="flex-1 lg:mb-0">
-          <h1 className='font-medium text-white text-[40px] xs:text-[50px] sm:text-[68px] md:text-[80px] lg:text-[100px] 2xl:text-[180px] leading-[110px] 2xl:leading-[160px]'>
-            M.Yaseen Raja
-          </h1>
+    <div className='parallax__content absolute top-0 sm:top-[2%] lg:top-[4%] w-full mx-auto lg:pl-[38vh] lg:pr-[30vh] xl:pl-96 xl:pr-72 2xl:px-40 3xl:px-60 flex flex-col lg:flex-row items-start z-10'>
+      <div className="flex-1 lg:mb-0 pl-4"> {/* Add padding to the left of the name */}
+        <h1 className='font-medium text-white text-[30px] xs:text-[40px] sm:text-[50px] md:text-[60px] lg:text-[70px] 2xl:text-[100px] leading-[60px] 2xl:leading-[80px]'>
+          M.Yaseen Raja
+        </h1>
+        <div className="lg:mt-5">
           <Position />
         </div>
+      </div>
       <div className="flex-1 flex justify-start lg:justify-end mt-20 sm:mt-59 md:mt-32 lg:mt-60"> {/* Adjusted margin-top for lower positioning */}
         <div className='font-bold text-[20px] sm:text-[30px] md:text-[36px] 2xl:text-[46px] sm:leading-[40px] md:leading-[50px] 2xl:leading-[60px] streaky-glow max-w-sm 2xl:max-w-lg text-white text-left'>
           I love To Make New Apps And Games!
